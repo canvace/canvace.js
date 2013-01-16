0b256d1ae593aee32b25fd893e9fc69e50d15427
Alberto La Rocca <a71104@gmail.com>
2013-01-16 15:28:50 +0100

    Code style.

--- src/Timing.js
+++ src/Timing.js
@@ -5,21 +5,22 @@
  * @static
  */
 Canvace.Timing = (function () {
-	var thisObject = {};
-
-	thisObject.now = (function () {
-		if (typeof window.performance !== 'object' || typeof window.performance.now !== 'function') {
-			return Date.now;
-		}
-
-		return function () {
-			return window.performance.now();
-		};
-	}());
-
-	thisObject.startTime = function () {
-		return window.mozAnimationStartTime || Canvace.Timing.now();
+	return {
+		now: (function () {
+			if (typeof window.performance !== 'object' || typeof window.performance.now !== 'function') {
+				return Date.now;
+			} else {
+				return window.performance.now;
+			}
+		})(),
+		startTime: (function () {
+			if ('mozAnimationStartTime' in window) {
+				return function () {
+					return mozAnimationStartTime;
+				};
+			} else {
+				return Canvace.Timing.now;
+			}
+		})()
 	};
-
-	return thisObject;
-}());
+})();
