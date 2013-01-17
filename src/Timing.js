ac09d3843efe43e4e98c1b3af2e923a8c918b206
Daniele Cocca <daniele.cocca@canvace.com>
2013-01-17 10:13:12 +0100

    Removing Canvace.Timing.animationStartTime().

--- src/Timing.js
+++ src/Timing.js
@@ -7,18 +7,6 @@
  * @static
  */
 Canvace.Timing = (function () {
-	var now = (function () {
-		if (typeof window.performance !== 'object' || typeof window.performance.now !== 'function') {
-			return function () {
-				return Date.now();
-			};
-		} else {
-			return function () {
-				return window.performance.now();
-			};
-		}
-	})();
-
 	return {
 		/**
 		 * This method returns a timestamp using `window.performance.now()`, if
@@ -27,35 +15,18 @@ Canvace.Timing = (function () {
 		 * @method now
 		 * @return {Number} A number indicating a timestamp.
 		 */
-		now: now,
-
-		/**
-		 * This method returns a timestamp that is suitable for being recorded
-		 * as the starting time of an animation. All subsequent
-		 * `Canvace.Timing.now()` calls should be considered relative to the
-		 * value returned by this method.
-		 *
-		 * @example
-		 *	var start = Canvace.Timing.animationStartTime();
-		 *	requestAnimationFrame(function callback(now) {
-		 *		var elapsed = (now - start);
-		 *
-		 *		// ...
-		 *
-		 *		requestAnimationFrame(callback);
-		 *	});
-		 *
-		 * @method animationStartTime
-		 * @return {Number} A number indicating a timestamp.
-		 */
-		animationStartTime: (function () {
-			if ('mozAnimationStartTime' in window) {
-				return function () {
-					return window.mozAnimationStartTime;
-				};
-			} else {
-				return now;
+		now: (function () {
+			if (!!window.performance) {
+				var now = Canvace.Polyfill.getPrefixedProperty(window.performance, 'now');
+				if (!!now) {
+					return function () {
+						return now.call(window.performance);
+					};
+				}
 			}
+			return function () {
+				return Date.now();
+			};
 		})()
 	};
 })();
