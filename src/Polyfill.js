1adca7f6e52e23bf058b37f73bbecffdeec8bcc4
Daniele Cocca <daniele.cocca@canvace.com>
2013-04-16 19:31:16 +0200

    Separating getPrefixedProperty and getPrefixedConstructor for Canvace.Polyfill.

--- src/Polyfill.js
+++ src/Polyfill.js
@@ -22,25 +22,50 @@ if (typeof Array.isArray !== 'function') {
 Canvace.Polyfill = (function () {
 	var prefixes = ['webkit', 'moz', 'ms', 'o'];
 
-	function getSinglePrefixedProperty(object, name) {
+	function getUnboundProperty(object, name) {
 		var capitalName = name.charAt(0).toUpperCase() + name.substr(1);
-
-		function bind(object, property) {
-			if (typeof property === 'function') {
-				return function () {
-					return property.apply(object, arguments);
-				};
-			}
-			return property;
-		}
 
 		for (var i in prefixes) {
 			var prefixedName = prefixes[i] + capitalName;
 			if (prefixedName in object) {
-				return bind(object, object[prefixedName]);
+				return object[prefixedName];
 			}
 		}
-		return bind(object, object[name]);
+
+		return object[name];
+	}
+
+	function getBoundProperty(object, name) {
+		var property = getUnboundProperty(object, name);
+
+		if (typeof property === 'function') {
+			return function () {
+				return property.apply(object, arguments);
+			};
+		}
+
+		return property;
+	}
+
+	function makePropertyGetter(getProperty) {
+		return function (object, property) {
+			if (arguments.length < 2) {
+				property = object;
+				object = window;
+			}
+
+			if (Array.isArray(property)) {
+				while (property.length > 1) {
+					var reference = getProperty(object, property.shift());
+
+					if (typeof reference !== 'undefined') {
+						return reference;
+					}
+				}
+			}
+
+			return getProperty(object, property.toString());
+		};
 	}
 
 	return {
@@ -50,11 +75,15 @@ Canvace.Polyfill = (function () {
 		 * Returns a reference to a possibly browser-prefixed property of a
 		 * given object.
 		 *
+		 * In case the property is a function, this function will return a
+		 * proxy function which transparently invokes such function with the
+		 * given object as the `this` reference.
+		 *
 		 * @example
 		 *	// Get a prefixed property from the `window` object.
 		 *	var requestAnimationFrame = Canvace.Polyfill.getPrefixedProperty('requestAnimationFrame');
 		 *
-		 *	// Get a prefixed property from a DOM element.
+		 *	// Get a prefixed function from a DOM element.
 		 *	var canvas = document.getElementById('canvas');
 		 *	var requestFullscreen = Canvace.Polyfill.getPrefixedProperty(canvas, ['requestFullscreen', 'requestFullScreen']);
 		 *
@@ -66,23 +95,29 @@ Canvace.Polyfill = (function () {
 		 * the same property name to try in order).
 		 * @return A reference to the requested property, or `undefined`.
 		 */
-		getPrefixedProperty: function (object, property) {
-			if (arguments.length < 2) {
-				property = object;
-				object = window;
-			}
+		getPrefixedProperty: makePropertyGetter(getBoundProperty),
 
-			if (Array.isArray(property)) {
-				while (property.length > 1) {
-					var reference = getSinglePrefixedProperty(object, property.shift());
-
-					if (typeof reference !== 'undefined') {
-						return reference;
-					}
-				}
-			}
-
-			return getSinglePrefixedProperty(object, property.toString());
-		}
+		/**
+		 * Returns a reference to a possibly browser-prefixed constructor of a
+		 * given object.
+		 *
+		 * In case the property is a function, this function will not be wrapped
+		 * by a proxy function - contrarily to what happens with the
+		 * `getPrefixedProperty` function - so that it can be used with the
+		 * `new` operator.
+		 *
+		 * @example
+		 *	// Get and use a prefixed constructor from the `window` object.
+		 *	var context = new Canvace.Polyfill.getPrefixedProperty('AudioContext');
+		 *
+		 * @method getPrefixedConstructor
+		 * @param [object] {Object} An optional reference to the object holding
+		 * the requested property. Defaults to the `window` object.
+		 * @param property {mixed} The constructor name to retrieve. This can be
+		 * a `String` (the property name) or an `Array` of `String`s (variants
+		 * of the same property name to try in order).
+		 * @return A reference to the requested constructor, or `undefined`.
+		 */
+		getPrefixedConstructor: makePropertyGetter(getUnboundProperty)
 	};
 }());
