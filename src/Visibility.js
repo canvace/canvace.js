79c7e8b76c6808be21c06d7e3072037958f234e6
Daniele Cocca <daniele.cocca@canvace.com>
2013-07-08 13:40:31 +0200

    Introducing the static Canvace.Visibility class.
    This renders Canvace.onVisibilityChange a wrapper for the methods
    exposed by Canvace.Visibility, which also lets the user query directly
    the visibility status and Page Visibility API support (instead of
    just listening to the visibilitychange event).

--- src/Visibility.js
+++ src/Visibility.js
@@ -19,34 +19,101 @@
  */
 
 /**
+ * Gives access to the Page Visibility API, if supported by the browser.
+ *
+ * @class Canvace.Visibility
+ * @static
+ */
+Canvace.Visibility = new function () {
+	var hidden;
+	var changeEvent;
+
+	if ('hidden' in document) {
+		hidden = 'hidden';
+		changeEvent = 'visibilitychange';
+	} else if ('webkitHidden' in document) {
+		hidden = 'webkitHidden';
+		changeEvent = 'webkitvisibilitychange';
+	} else if ('mozHidden' in document) {
+		hidden = 'mozHidden';
+		changeEvent = 'mozvisibilitychange';
+	} else if ('msHidden' in document) {
+		hidden = 'msHidden';
+		changeEvent = 'msvisibilitychange';
+	}
+
+	/**
+	 * Tells if the browser supports the Page Visibility API.
+	 *
+	 * @method isSupported
+	 * @return {Boolean} A value indicating whether such support is available.
+	 */
+	this.isSupported = function () {
+		return (typeof hidden !== 'undefined');
+	};
+
+	/**
+	 * Queries the visibility status of the document.
+	 *
+	 * @method isVisible
+	 * @return {Boolean} A value indicating if the document is visible or not.
+	 *   Defaults to the boolean `true` if the Page Visibility API is not
+	 *   supported.
+	 */
+	this.isVisible = function () {
+		if (!this.isSupported()) {
+			return true;
+		}
+
+		return (!document[hidden]);
+	};
+
+	/**
+	 * Registers an event listener for the `visibilitychange` event.
+	 * This method defaults to a no-operation if the Page Visibility API is not
+	 * supported.
+	 *
+	 * @method onChange
+	 * @param callback {Function} The callback function to invoke when a
+	 *   `visibilitychange` event is triggered.
+	 * @return {Function} A remover method which unregisters the event listener.
+	 */
+	this.onChange = (function (isSupported) {
+		if (!isSupported) {
+			return function () {
+				return function () {
+				};
+			};
+		}
+
+		return function (callback) {
+			function eventListener() {
+				callback(document[hidden]);
+			}
+
+			document.addEventListener(changeEvent, eventListener, false);
+			return function () {
+				document.removeEventListener(changeEvent, eventListener, false);
+			};
+		};
+	})(this.isSupported());
+};
+
+/**
  * Adds an event listener for the "visibility change" event, if supported by the
  * browser.
  *
  * @method onVisibilityChange
- * @param callback {Function} TODO
- * @return {Boolean} A boolean value indicating whether the browser supports the
- *	event or not.
+ * @param callback {Function} The callback function to invoke when a
+ *   `visibilitychange` event is triggered.
+ * @return {Mixed} A function which removes the event listener if the Page
+ *   Visibility API is supported, or the boolean `false` otherwise
  * @for Canvace
  */
 Canvace.onVisibilityChange = function (callback) {
-	if ('hidden' in document) {
-		document.addEventListener('visibilitychange', function () {
-			callback(document.hidden);
-		}, false);
-	} else if ('webkitHidden' in document) {
-		document.addEventListener('webkitvisibilitychange', function () {
-			callback(document.webkitHidden);
-		}, false);
-	} else if ('mozHidden' in document) {
-		document.addEventListener('mozvisibilitychange', function () {
-			callback(document.mozHidden);
-		}, false);
-	} else if ('msHidden' in document) {
-		document.addEventListener('msvisibilitychange', function () {
-			callback(document.msHidden);
-		}, false);
-	} else {
-		return false;
+	if (Canvace.Visibility.isSupported()) {
+		return Canvace.Visibility.onChange(callback);
 	}
-	return true;
+	return false;
 };
+
