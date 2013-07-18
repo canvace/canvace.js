b3cf0c0f3c2eb18b1191239f65a27e55fd9ff9a5
Daniele Cocca <daniele.cocca@canvace.com>
2013-07-18 21:36:06 +0200

    Adding the documentation.

---
+++ api.js
@@ -0,0 +1,53 @@
+YUI.add("yuidoc-meta", function(Y) {
+   Y.YUIDoc = { meta: {
+    "classes": [
+        "Canvace",
+        "Canvace.Ajax",
+        "Canvace.Ajax.Request",
+        "Canvace.Animator",
+        "Canvace.Animator.Easing",
+        "Canvace.Astar",
+        "Canvace.Astar.Node",
+        "Canvace.Audio",
+        "Canvace.Audio.SourceNode",
+        "Canvace.Buckets",
+        "Canvace.Buckets.Entity",
+        "Canvace.DebugEffect",
+        "Canvace.Heap",
+        "Canvace.Keyboard",
+        "Canvace.List",
+        "Canvace.List.Accessor",
+        "Canvace.Loader",
+        "Canvace.Matrix",
+        "Canvace.Mouse",
+        "Canvace.MultiSet",
+        "Canvace.ParametricStateMachine",
+        "Canvace.Polyfill",
+        "Canvace.RenderLoop",
+        "Canvace.Renderer",
+        "Canvace.RumbleEffect",
+        "Canvace.Stage",
+        "Canvace.Stage.Entity",
+        "Canvace.Stage.Instance",
+        "Canvace.Stage.Range",
+        "Canvace.StageRenderer",
+        "Canvace.StateMachine",
+        "Canvace.TileMap",
+        "Canvace.TileMap.Tile",
+        "Canvace.Timing",
+        "Canvace.View",
+        "Canvace.View.Synchronizer",
+        "Canvace.Visibility"
+    ],
+    "modules": [
+        "Canvace"
+    ],
+    "allModules": [
+        {
+            "displayName": "Canvace",
+            "name": "Canvace",
+            "description": "The main namespace object of Canvace's Javascript client library is named\n`Canvace`. Every Canvace class must be accessed from that object."
+        }
+    ]
+} };
+});
